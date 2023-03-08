5aaa6c58cba9d90a64cdaedfe8e67426388a2adb
Ivan Apostolovski <119892695+IvanXh0@users.noreply.github.com>
2023-03-08 23:53:32 +0100

    cleaned up unnecessary code

--- Homework-2-Events/index.js
+++ Homework-2-Events/index.js
@@ -4,21 +4,17 @@ class MyEmitter extends EventEmitter {}
 
 const emitter = new MyEmitter();
 
+const createAndGreetStudents = () => {
+  for (let i = 1; i <= 5; i++) {
+    const newStudents = new Student();
+    emitter.emit("greet", newStudents);
+  }
+};
+
 emitter.on("greet", (student) => {
   console.log(
     `Hello ${student.firstName} ${student.lastName} aged ${student.age}, welcome to the ${student.academy} academy.`
   );
 });
-
-const createAndGreetStudents = () => {
-  const students = [];
-
-  for (let i = 1; i <= 5; i++) {
-    const newStudents = new Student();
-    students.push(newStudents);
-    emitter.emit("greet", newStudents);
-  }
-  return students;
-};
 
 createAndGreetStudents();
